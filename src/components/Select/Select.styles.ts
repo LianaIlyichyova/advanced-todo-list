27e28756863a366ee125d1b492154dbbb5da5aec
IlyichyovaLiana <liana@ottstream.live>
2025-05-19 02:34:09 +0400

    fix: styles corrections

--- src/components/Select/Select.styles.ts
+++ src/components/Select/Select.styles.ts
@@ -5,15 +5,11 @@ import { Select } from "antd";
 const StyledSelect = styled(Select)`
   max-width: 300px;
   .ant-select-arrow,
-  .ant-select-multiple
-    .ant-select-selection-overflow
-    .ant-select-selection-item-remove,
-  .ant-select-item-option-state {
-    color: inherit;
-  }
-
-  &:hover .ant-select-clear {
-    color: inherit;
+  .ant-select-selection-item-remove,
+  .ant-select-item-option-state,
+  .ant-select-clear,
+  .antd-icon-close {
+    color: inherit !important;
   }
 `;
 
--- src/styles/styled.d.ts
+++ src/styles/styled.d.ts
@@ -3,14 +3,11 @@ import "styled-components";
 declare module "styled-components" {
   export interface DefaultTheme {
     token: {
-      colorBgTertiary: string;
       colorPrimaryBg: string;
       colorBgContainer: string;
       colorIcon: string;
-      fontSizeIcon: number | string;
       colorIconHover: string;
       colorTextPrimary: string;
-      colorBgContainer: string;
     };
   }
 }
